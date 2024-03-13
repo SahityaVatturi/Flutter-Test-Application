4a4c2c9460c12ff2e26a46186fdd56294887dddb
SahityaVatturi <sahithyavatturi28@gmail.com>
2024-03-13 21:24:39 +0530

    Implemented task1 and task2

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,120 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('GitHub Repositories'),
+        ),
+        body: GitHubRepoList(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class GitHubRepoList extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _GitHubRepoListState createState() => _GitHubRepoListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GitHubRepoListState extends State<GitHubRepoList> {
+  final String user = 'freeCodeCamp';
+  final String apiUrl = 'https://api.github.com/users/freeCodeCamp/repos';
+  late Future<List<Map<String, dynamic>>> repos;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    repos = fetchRepositories();
+  }
+
+  Future<List<Map<String, dynamic>>> fetchRepositories() async {
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> reposData = json.decode(response.body);
+
+      // Fetch last commit information for each repository
+      final List<Map<String, dynamic>> repositories = [];
+      for (var repo in reposData) {
+        final lastCommit = await fetchLastCommit(repo['name']);
+        repositories.add({
+          'repo': repo,
+          'lastCommit': lastCommit,
+        });
+      }
+
+      return repositories;
+    } else {
+      throw Exception('Failed to load repositories');
+    }
+  }
+
+  Future<Map<String, dynamic>> fetchLastCommit(String repoName) async {
+    final commitUrl = 'https://api.github.com/repos/$user/$repoName/commits';
+    final response = await http.get(Uri.parse(commitUrl));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> commits = json.decode(response.body);
+
+      if (commits.isNotEmpty) {
+        // Return the last commit information if available
+        return commits[0];
+      } else {
+        // Return an empty map if there are no commits
+        return {};
+      }
+    } else {
+      print('Failed to load last commit for $repoName');
+      // Return an empty map or handle the error accordingly
+      return {};
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return FutureBuilder(
+      future: repos,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else {
+          final List<Map<String, dynamic>> repositories =
+              snapshot.data as List<Map<String, dynamic>>;
+
+          return ListView.builder(
+            itemCount: repositories.length,
+            itemBuilder: (context, index) {
+              final repoData = repositories[index];
+              final repo = repoData['repo'];
+              final lastCommit = repoData['lastCommit'];
+
+              return ListTile(
+                title: Text(repo['name']),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(repo['description'] ?? 'No description available'),
+                    if (lastCommit.isNotEmpty)
+                      Text('Last Commit: ${lastCommit['commit']['message']}'),
+                  ],
+                ),
+                // You can customize the list tile further based on your needs
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
